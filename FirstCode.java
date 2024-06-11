265f1ef46281a52ac9b2bc97e5d2cf8051d54af3
saifullah <saifulah.khan1203@gmail.com>
2024-06-11 20:39:02 +0500

    Added input functionality

--- FirstCode.java
+++ FirstCode.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class FirstCode{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter you Name: ");
+        String name = scan.nextLine();
+
+        System.out.println("Name Entered: " + name);
     }
 }
